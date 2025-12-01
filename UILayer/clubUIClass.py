fa2881eea54951f8832a98b2b060835a5c806c64
Konnigard <hakongardargauks@gmail.com>
2025-12-01 16:28:01 +0000

    ClassCreation

---
+++ UILayer/clubUIClass.py
@@ -0,0 +1,5 @@
+class clubUI:
+    def __init__(self):
+        self.name = input("Please enter the name of the Club: ")
+        self.hometown = input("Please enter the home town of the club: ")
+        self.country = input("Please enter the country the club originates: ")
---
+++ UILayer/playerUIClass.py
@@ -0,0 +1,9 @@
+class playerUI:
+    def __init__(self):
+        self.name = input("Please Enter the Players Full Name: ")
+        self.username = input("Please Enter the Players Username: ")
+        self.DoB = input("Plese Enter the Players Date of Birth: ")
+        self.address = input("Please Enter the Players Home Address: ")
+        self.phoneNumber = input("Please Enter the Players Phone Number: ")
+        self.email = input("Please Enter the Players Email: ")
+        self.link = input("Please Enter the Players Link: ")
