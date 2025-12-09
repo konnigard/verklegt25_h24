2272d77621eccd57407e5deb31b5a7fe6ead96b7
Konnigard <hakongardargauks@gmail.com>
2025-12-09 12:26:28 +0000

    Validate-ar að input sé rétt

--- LogicLayer/TeamLogic.py
+++ LogicLayer/TeamLogic.py
@@ -18,7 +18,17 @@ class TeamLogicClass:
     def validateAndAddNewTeam(self, newTeam: Team) :
         """ Checks to see if the team exist already """
         # 1) Check if all fileds are valid 
-        
+        while True:
+            if Team.validateTeamID(newTeam.teamID) == True:
+                if Team.validateTeamName(newTeam.teamName) == True:
+                    if Team.validateTeamClub(newTeam.teamClub) == True:
+                        break
+                    else:
+                        return "Team Club is invalid, Regestration Failed"
+                else:
+                    return "Team Name is invalid, Regestration Failed"
+            else:
+                return "Team ID is invalid, Regestration Failed"
         # 2) check if all uniqe things are uniqe 
         #   - This will require reading all teams from file
         allTeams: list[Team]= self.datawrapper.loadAllTeams()
