9accfa89fec7f890920c15c8597b5d4b773a5ae7
Konnigard <hakongardargauks@gmail.com>
2025-12-09 15:39:46 +0000

    Tournament Model Komið

---
+++ Models/tournamentModel.py
@@ -0,0 +1,10 @@
+class Tournament:
+    def __inti__(self, tourName, tourGame, tourLoc, tourStartDate, tourEndDate, tourContact, tourPhone, tourEmail):
+        self.name: str = tourName
+        self.game: str = tourGame
+        self.location: str = tourLoc
+        self.startDate: str = tourStartDate
+        self.endDate: str = tourEndDate
+        self.contact: str = tourContact
+        self.contactPhone: str = tourPhone
+        self.conactEmail: str = tourEmail
